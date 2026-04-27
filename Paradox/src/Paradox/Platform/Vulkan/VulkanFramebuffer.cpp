e8879a4f78aa538c72e9be92b89a17847fc059c5
Stealth <67294868+ImNotStealth@users.noreply.github.com>
2026-04-27 23:20:00 +0200

    Forgot to commit those

---
+++ Paradox/src/Paradox/Platform/Vulkan/VulkanFramebuffer.cpp
@@ -0,0 +1,17 @@
+﻿#include "pxpch.h"
+#include "VulkanFramebuffer.h"
+
+#include "Paradox/Platform/Vulkan/VulkanDevice.h"
+
+namespace Paradox
+{
+	VulkanFramebuffer::VulkanFramebuffer(const FramebufferProperties& props)
+	{
+
+	}
+
+	VulkanFramebuffer::~VulkanFramebuffer()
+	{
+		vkDestroyFramebuffer(VulkanDevice::Get().GetDevice(), m_Framebuffer, nullptr);
+	}
+}
---
+++ Paradox/src/Paradox/Platform/Vulkan/VulkanFramebuffer.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "Paradox/Renderer/Framebuffer.h"
+
+#include <vulkan/vulkan.h>
+
+namespace Paradox
+{
+	class VulkanFramebuffer : public Framebuffer
+	{
+	public:
+		VulkanFramebuffer(const FramebufferProperties& props);
+		~VulkanFramebuffer();
+
+		void OnResize(uint32_t width, uint32_t height) override {}
+		FramebufferProperties& GetProperties() override { return m_Props; }
+
+	private:
+		FramebufferProperties m_Props;
+
+		VkFramebuffer m_Framebuffer;
+	};
+}
